59d15b0757c4e3760b18391094118a7808a9f298
Arif Hossain <arif434@gmail.com>
2022-06-07 16:41:36 -0400

    Update sampletest.py

--- sampletest/sampletest.py
+++ sampletest/sampletest.py
@@ -3,14 +3,14 @@ import chromedriver_autoinstaller
 from selenium.webdriver import Chrome
 
 class View():
-    def __init__(self, driver=None):
-        # if driver is None:
-        #     driver = {}
-        # else:
-            self.driver = driver
-    chromedriver_autoinstaller.install()
+
+    def __init__(self, driver=None,options=None):
+        chromedriver_autoinstaller.install()
+        options = webdriver.ChromeOptions()
+        #options.set_headless = True
+        options.headless = True
+        self.driver = webdriver.Chrome(options=options)
     def setUp(self):
-        self.driver = webdriver.Chrome()
         self.driver.get("http://www.python.org")
         assert "Python" in self.driver.title
         print(self.driver.title)
