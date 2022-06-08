87cfc2ea9d68b9a54a1b95e0645ef8cb7c8d4fa2
Arif Hossain <arif434@gmail.com>
2022-06-08 18:10:29 -0400

    Update sampletest.py

--- sampletest/sampletest.py
+++ sampletest/sampletest.py
@@ -1,10 +1,12 @@
 from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import chromedriver_autoinstaller
 from selenium.webdriver import Chrome
 
 class View():
 
     def __init__(self, driver=None,options=None):
+        self.driver = webdriver.Remote(command_executor='http://172.17.0.1:4444/wd/hub', desired_capabilities=DesiredCapabilities.FIREFOX)
         chromedriver_autoinstaller.install()
         options = webdriver.ChromeOptions()
         #options.set_headless = True
