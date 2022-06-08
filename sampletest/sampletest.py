aca2380ead5c59184481507306ac5438391cfb84
Arif Hossain <arif434@gmail.com>
2022-06-08 18:56:10 -0400

    Update sampletest.py

--- sampletest/sampletest.py
+++ sampletest/sampletest.py
@@ -1,16 +1,14 @@
 from selenium import webdriver
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-#import chromedriver_autoinstaller
+import chromedriver_autoinstaller
 from selenium.webdriver import Chrome
 
 class View():
 
     def __init__(self, driver=None,options=None):
-        self.driver = webdriver.Remote(command_executor='http://172.17.0.1:4444', desired_capabilities=DesiredCapabilities.CHROME)
-        #chromedriver_autoinstaller.install()
+        chromedriver_autoinstaller.install()
         options = webdriver.ChromeOptions()
-        options.set_headless = True
-        #options.headless = True
+        #options.set_headless = True
+        options.headless = True
         self.driver = webdriver.Chrome(options=options)
     def setUp(self):
         self.driver.get("http://www.python.org")
